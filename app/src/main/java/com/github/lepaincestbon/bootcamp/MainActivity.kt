2cb99894e6a38685461ae9d1bd12c68cacc859d8
Paul Coral <paul.coral@epfl.ch>
2021-01-17 23:36:56 +0100

    Implement Asynchronicity

--- app/src/main/java/com/github/lepaincestbon/bootcamp/MainActivity.kt
+++ app/src/main/java/com/github/lepaincestbon/bootcamp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.github.lepaincestbon.bootcamp
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.StrictMode
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -14,11 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // this allow using networking on the main thread (not allowed by default on Android)
-        // TODO: Remove this when networking is async
-        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitNetwork().build())
-
     }
 
     fun sendMessage(view: View) {
